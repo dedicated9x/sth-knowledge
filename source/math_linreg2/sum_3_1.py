9521e80e43085232999627f17ecde10b918569fe
Brysch Pawel <pawel.brysch@gmail.com>
2020-11-06 16:19:07 +0100

    Wszystkie 3 przeszly

--- source/math_linreg2/sum_3_1.py
+++ source/math_linreg2/sum_3_1.py
@@ -5,9 +5,11 @@ import scipy.stats
 import sklearn.metrics
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import Ridge
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import KFold
+from sklearn.linear_model import Ridge, LinearRegression
+from sklearn.decomposition import PCA
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import KFold, GridSearchCV
 
 def calculate_degrees_freedom(X, alpha):
     inner_factor = np.linalg.inv(X.T.__matmul__(X) + alpha * np.identity(X.shape[1]))
@@ -35,6 +37,11 @@ X_scaled, y_scaled = df_train_scaled[:, :-1], df_train_scaled[:, -1]
 # rgr = Ridge(alpha=36.6)
 # rgr.fit(X_scaled, y_scaled)
 
+#TODO ujednolicic
+
+"""####################
+RR
+####################"""
 alpha_range = [0.1, 4.3, 11.2, 22.8, 36.6, 74.4, 151.1, 388.8, 1000.0]
 param_grid = dict(alpha=alpha_range)
 cv = KFold(n_splits=10)
@@ -42,15 +49,60 @@ scorer = sklearn.metrics.make_scorer(sklearn.metrics.mean_squared_error)
 grid = GridSearchCV(Ridge(), scoring=scorer, param_grid=param_grid, cv=cv)
 
 grid.fit(X_scaled, y_scaled)
-
-
-
 results = pd.DataFrame.from_dict(grid.cv_results_)[['param_alpha', 'mean_test_score', 'std_test_score']]
 
-# plot
+
+"""https://scikit-learn.org/stable/tutorial/statistical_inference/putting_together.html"""
+
+"""####################
+PCA
+####################"""
+pca = PCA()
+linreg = LinearRegression()
+pipe = Pipeline(steps=[('pca', pca), ('linreg', linreg)])
+param_grid = {
+    'pca__n_components': [1, 2, 3, 4, 5, 6, 7, 8],
+}
+scorer = sklearn.metrics.make_scorer(sklearn.metrics.mean_squared_error)
+search = GridSearchCV(pipe, scoring=scorer, param_grid=param_grid, cv=cv)
+search.fit(X_scaled, y_scaled)
+
+results2 = pd.DataFrame.from_dict(search.cv_results_)[['param_pca__n_components', 'mean_test_score', 'std_test_score']]
+
+
+"""####################
+PLS
+####################"""
+n_components_range = [1, 2, 3, 4, 5, 6, 7, 8]
+param_grid = dict(n_components=n_components_range)
+cv = KFold(n_splits=10)
+scorer = sklearn.metrics.make_scorer(sklearn.metrics.mean_squared_error)
+grid = GridSearchCV(PLSRegression(), scoring=scorer, param_grid=param_grid, cv=cv)
+grid.fit(X_scaled, y_scaled)
+
+results3 = pd.DataFrame.from_dict(grid.cv_results_)[['param_n_components', 'mean_test_score', 'std_test_score']]
+
+
+
+
+
+""" linreg wytrenowany na PCA od razu (!) predyktuje dobrze"""
+# pca_best = PCA(n_components=8)
+# pca_best.fit(X_scaled)
+# X_pca = pca_best.transform(X_scaled)
+# rgr = LinearRegression()
+# rgr.fit(X_pca, y_scaled)
+
+
+
+
+
+
+
 
 """test"""
 # rgr.score(X_scaled, y_scaled)
+# rgr.score(X_pca, y_scaled)
 # z1 = pd.DataFrame().assign(
 #     rec=rgr.predict(X_scaled),
 #     exp=y_scaled
