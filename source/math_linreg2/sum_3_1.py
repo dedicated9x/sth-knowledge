55b948e9213fbd77eaf58994d96ea055e260074f
Brysch Pawel <pawel.brysch@gmail.com>
2020-11-15 19:14:47 +0100

    bedziemy zmieniac perform cv

--- source/math_linreg2/sum_3_1.py
+++ source/math_linreg2/sum_3_1.py
@@ -25,6 +25,36 @@ scaler.fit(df_train)
 df_train_scaled = scaler.transform(df_train)
 X_scaled, y_scaled = df_train_scaled[:, :-1], df_train_scaled[:, -1]
 
+""" ##############################################################
+Zheng Log method implementation (for PCR_ord)
+###############################################################"""
+def compute_sort_keys_from_tvalues(X):
+    if_passed = False
+    while if_passed is False:
+        try:
+            X_pca = PCA(n_components=X.shape[1]).fit_transform(X)
+        except np.linalg.LinAlgError:
+            pass
+        else:
+            if_passed = True
+
+    tvalues = sm.OLS(y_scaled, sm.add_constant(X_pca)).fit().tvalues[1:]
+    tvalues_square = tvalues * tvalues
+    return np.argsort(-tvalues_square)
+
+SORT_KEYS = compute_sort_keys_from_tvalues(X_scaled)
+
+class ZhengLohTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, n_components=None):
+        self.n_components = n_components
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        global SORT_KEYS
+        return X[:, SORT_KEYS][:, 0:self.n_components]
+
 
 """ ##############################################################
 Cross validation
@@ -39,9 +69,9 @@ def perform_cv(model, param_name, param_range):
     return results
 
 
-# results_RR = perform_cv(Ridge(), 'alpha', [0.1, 4.3, 11.2, 22.8, 36.6, 74.4, 151.1, 388.8, 1000.0])
-# results_PCR = perform_cv(Pipeline(steps=[('pca', PCA()), ('linreg', LinearRegression())]), 'pca__n_components', [1, 2, 3, 4, 5, 6, 7, 8])
-# results_PLS = perform_cv(PLSRegression(), 'n_components', [1, 2, 3, 4, 5, 6, 7, 8])
+results_RR = perform_cv(Ridge(), 'alpha', [0.1, 4.3, 11.2, 22.8, 36.6, 74.4, 151.1, 388.8, 1000.0])
+results_PCR = perform_cv(Pipeline(steps=[('pca', PCA()), ('linreg', LinearRegression())]), 'pca__n_components', [1, 2, 3, 4, 5, 6, 7, 8])
+results_PCRord = perform_cv(Pipeline(steps=[('zlt', ZhengLohTransformer()), ('linreg', LinearRegression())]), 'zlt__n_components', [1, 2, 3, 4, 5, 6, 7, 8])
 
 
 """ ##############################################################
@@ -54,10 +84,10 @@ def calculate_degrees_freedom(X, alpha):
     return np.trace(hat_matrix)
 
 
-# results_RR['param_alpha'] = results_RR['param_alpha'].apply(lambda x: calculate_degrees_freedom(X_scaled, x))
-# results_RR = results_RR.rename(columns={'param_alpha': 'param_df'})
-# results_RR = results_RR.sort_values(by=['param_df'])
-# results_RR = results_RR.reset_index(drop=True)
+results_RR['param_alpha'] = results_RR['param_alpha'].apply(lambda x: calculate_degrees_freedom(X_scaled, x))
+results_RR = results_RR.rename(columns={'param_alpha': 'param_df'})
+results_RR = results_RR.sort_values(by=['param_df'])
+results_RR = results_RR.reset_index(drop=True)
 
 
 """ ##############################################################
@@ -79,112 +109,8 @@ def plot_results(ax, results, param_name, title):
     ax.set_title(title)
 
 
-# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
-# plot_results(ax1, results_RR, 'param_df', 'ridge')
-# plot_results(ax2, results_PCR, 'param_pca__n_components', 'pcr')
-# plot_results(ax3, results_PLS, 'param_n_components', 'pls')
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+plot_results(ax1, results_RR, 'param_df', 'ridge')
+plot_results(ax2, results_PCR, 'param_pca__n_components', 'pcr')
+plot_results(ax3, results_PCRord, 'param_zlt__n_components', 'pcr_ord')
 
-""" ##############################################################
-###############################################"""
-
-# def perform_pca_and_sort_by_tvalues(X):
-#     if_passed = False
-#     while if_passed is False:
-#         try:
-#             X_pca = PCA(n_components=X.shape[1]).fit_transform(X)
-#         except np.linalg.LinAlgError:
-#             pass
-#         else:
-#             if_passed = True
-#
-#     tvalues = sm.OLS(y_scaled, sm.add_constant(X_pca)).fit().tvalues[1:]
-#     tvalues_square = tvalues * tvalues
-#     X_pca_sorted = X_pca[:, np.argsort(-tvalues_square)]
-#     return X_pca_sorted
-
-
-def perform_pca_and_sort_by_tvalues(X):
-    if_passed = False
-    while if_passed is False:
-        try:
-            X_pca = PCA(n_components=X.shape[1]).fit_transform(X)
-        except np.linalg.LinAlgError:
-            pass
-        else:
-            if_passed = True
-
-    tvalues = sm.OLS(y_scaled, sm.add_constant(X_pca)).fit().tvalues[1:]
-    tvalues_square = tvalues * tvalues
-    return np.argsort(-tvalues_square)
-
-
-# X_pca = PCA(n_components=8).fit_transform(X_scaled)
-# tvalues = sm.OLS(y_scaled, sm.add_constant(X_pca)).fit().tvalues[1:]
-# tvalues_square = tvalues * tvalues
-# X_pca_sorted = X_pca[:, np.argsort(-tvalues_square)]
-# X_pca_sorted = perform_pca_and_sort_by_tvalues(X_scaled)
-indices = perform_pca_and_sort_by_tvalues(X_scaled)
-
-
-class MyTemplateEstimator(BaseEstimator):
-    def __init__(self, n_components=1):
-        self.n_components = n_components
-
-    def fit(self, X, y):
-            self.linreg = LinearRegression()
-            self.linreg.fit(perform_pca_and_sort_by_tvalues(X)[:, 0:self.n_components], y)
-            return self
-
-    def predict(self, X):
-            return self.linreg.predict(perform_pca_and_sort_by_tvalues(X)[:, 0:self.n_components])
-
-
-class MyTemplateTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, n_components=1):
-        self.n_components = n_components
-
-    def fit(self, X, y=None):
-            return self
-
-    def transform(self, X):
-        global indices
-        # return perform_pca_and_sort_by_tvalues(X)[:, 0:self.n_components]
-        return X[:, indices][:, 0:self.n_components]
-
-
-test1 = MyTemplateTransformer(n_components=8)
-test1.fit(X_scaled)
-z1 = test1.transform(X_scaled)
-
-
-
-# sth1 = MyTemplateEstimator(n_components=3)
-# sth1.fit(X_scaled, y_scaled)
-# sth1.predict(X_scaled)
-
-
-cv = KFold(n_splits=10)
-scorer = sklearn.metrics.make_scorer(sklearn.metrics.mean_squared_error)
-grid = GridSearchCV(Pipeline(steps=[('pca', MyTemplateTransformer()), ('linreg', LinearRegression())]), scoring=scorer, param_grid={'pca__n_components': [1, 2, 3, 4, 5, 6, 7, 8]}, cv=cv)
-grid.fit(X_scaled, y_scaled)
-
-
-
-
-
-
-
-
-perform_cv(Pipeline(steps=[('pca', MyTemplateTransformer()), ('linreg', LinearRegression())]), 'pca__n_components', [1, 2, 3, 4, 5, 6, 7, 8])
-
-# results_RR = perform_cv(Ridge(), 'alpha', [0.1, 4.3, 11.2, 22.8, 36.6, 74.4, 151.1, 388.8, 1000.0])
-# results_PCR = perform_cv(Pipeline(steps=[('pca', PCA()), ('linreg', LinearRegression())]), 'pca__n_components', [1, 2, 3, 4, 5, 6, 7, 8])
-# results_PLS = perform_cv(PLSRegression(), 'n_components', [1, 2, 3, 4, 5, 6, 7, 8])
-
-
-
-
-""" test srotowania """
-# list(np.var(X_pca, axis=0))
-# list(tvalues_square)
-# list(np.var(X_pca_sorted, axis=0))
