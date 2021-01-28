04d5a3a691cba3bb877f7e19d7100a65288daca0
Brysch Pawel <pawel.brysch@gmail.com>
2021-01-28 12:40:54 +0100

    klas start

--- source/math_linreg2/sad2_proj2.py
+++ source/math_linreg2/sad2_proj2.py
@@ -2,7 +2,12 @@ import pomegranate as pg
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pathlib as pl
 import random
+
+
+root_path = pl.Path(r'C:\Users\devoted\Desktop\sad2_output')
+text_output_path = root_path.joinpath('txt_output.txt')
 
 """#####################
 Model initialization
@@ -77,6 +82,23 @@ def calculate_probabilities_T(nominators_T_, nominators_F_):
 
 
 probabilities_T = calculate_probabilities_T(nominators_T, nominators_F)
+
+with open(text_output_path, 'w') as outfile:
+    outfile.write('Task 1.\n')
+
+    outfile.write('Equations and laws we will use:\n')
+    outfile.write('P(A|BCD) = P(ABCD)/P(BCD) (cond. probability) \n')
+    outfile.write('P(X1, X2, ..., Xn) = product_of_probabilities P(X_i|parents(X_i)) (Bayesian Network\'s rule)\n')
+    outfile.write("(a' / b' = a / b and a' + b' = 1) => ( a' = a/(a+b) and b' = b/(a+b) ) (renormalization)\n")
+
+    outfile.write('\n')
+    for k, v in nominators_T.items():
+        outfile.write("P({},S=T,W=T) =  {:.2f} / some_denominator\n".format(k, v))
+    outfile.write('\n')
+    for k, v in probabilities_T.items():
+        outfile.write("P({},S=T,W=T) =  {:.2f}\n".format(k, v))
+    outfile.write('\n')
+
 """#######################################################################################
 2. Implement the Gibbs sample and draw sequence of 1000
 #######################################################################################"""
@@ -117,12 +139,20 @@ def draw_sequence(length):
     df = pd.DataFrame().assign(rain=rain_seq, cloudy=cloudy_seq)
     return df
 
-df = draw_sequence(1000)
+df = draw_sequence(100)
+
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 2.\n')
+    outfile.write('The Gibbs sampler has been implemented.\n\n')
 
 """#######################################################################################
 3. Estimate marginal probability of rain
 #######################################################################################"""
-marginal_R_T = df['rain'].value_counts(normalize=True)['T']
+marginal_R_T_sample100 = df['rain'].value_counts(normalize=True)['T']
+
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 3.\n')
+    outfile.write('P(R=T|S=T,W=T)_sample100 = {:.2f}\n\n'.format(marginal_R_T_sample100 ))
 
 
 """#######################################################################################
@@ -153,18 +183,25 @@ def plot_frequencies(chain1_, chain2_):
         ax.scatter(t_list, freq_rain_list, color='b', label="p(R=T)")
         ax.scatter(t_list, freq_cloudy_list, color='k', label="p(C=T)")
         ax.set_xlim(0, 1000)
-        ax.set_xlabel(label)
+        ax.set_ylim(0, 1)
+        ax.set_xlabel(label, labelpad=-10)
         ax.legend(loc='upper right')
 
-# plot_frequencies(chain1, chain2)
+    fig.savefig(root_path.joinpath('task5_rel_freq.png'))
+
+plot_frequencies(chain1, chain2)
+
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 5.\n')
+    outfile.write('I think n=300 will be good for burn-in.\n\n')
 
 """#######################################################################################
 6. Apply the Gelman test. Plot potential scale reduction factor. Suggest a burn-in time.
 #######################################################################################"""
 """ wybor -> doubled_n = 500 """
-def convert_to_int(chain):
+def convert_to_float(chain):
     # Obliczenia wymagają wartosci liczbowych
-    return chain.applymap(lambda x: {'T': 1, 'F': 0}[x])
+    return chain.applymap(lambda x: {'T': 1.0, 'F': 0.0}[x])
 
 
 def calculate_psrf(doubled_n, complete_chain1, complete_chain2):
@@ -192,56 +229,85 @@ def plot_psrf_list(psrf_list_, doubled_n_list_):
     fig, ax1 = plt.subplots(1, 1)
     ax1.set_xlim(0, 2000)
     ax1.set_ylim(0.9, 2.5)
+    ax1.set_xlabel('n*')
+    ax1.set_ylabel('PSRF')
     ax1.scatter(doubled_n_list_, psrf_list_)
 
+    fig.savefig(root_path.joinpath('task6_psrf.png'))
 
-chain1_int, chain2_int = [convert_to_int(chain) for chain in [chain1, chain2]]
+
+
+chain1_float, chain2_float = [convert_to_float(chain) for chain in [chain1, chain2]]
 doubled_n_list = [2 * round(elem / 2) for elem in np.logspace(1, 4.68, 150)]
-psrf_list = [calculate_psrf(n, chain1_int, chain2_int) for n in doubled_n_list]
-# plot_psrf_list(psrf_list, doubled_n_list)
+psrf_list = [calculate_psrf(n, chain1_float, chain2_float) for n in doubled_n_list]
+plot_psrf_list(psrf_list, doubled_n_list)
+
+
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 6.\n')
+    outfile.write('I wrote n* instead of n, because of the Gelman test implementation, where n has been assigned to the half the length.\n')
+    outfile.write('Therefore n*=2n indeed.\n')
+    outfile.write('I used algorithm directly from: "General Methods for Monitoring Convergence of Iterative Simulations. Brooks, Gelman. 1997".\n')
+    outfile.write('I think 2n=500 (n=250) will be good for burn-in.\n')
+    outfile.write('Taking into account the result of the task 5 my choice, for burning in point is n=max(300, 250)=300.\n\n')
+    
+
+"""#######################################################################################
+7. Plot autocorrelation. Suggest an interval.
+#######################################################################################"""
+""" wybor -> maxlags = 20 """
+def plot_acorr(chain):
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    for ax, var_full, var_abrr in zip([ax1, ax2], ['rain', 'cloudy'], ['R', 'C']):
+        ax.acorr(chain[var_full].values, maxlags=30)
+        ax.set_xlabel('k, ' + var_full + f' ({var_abrr})', labelpad=-5)
+        ax.set_ylabel('lag k')
+        ax.set_xlim(0, 30)
+    fig.savefig(root_path.joinpath('task7_corr.png'))
+
+plot_acorr(chain1_float)
+
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 7.\n')
+    outfile.write('I think interval=20 will be good for thinning.\n\n')
 
 
 """#######################################################################################
-7. Autocorrelation
+8. Re-estimate marginal probability of rain
 #######################################################################################"""
+df_v2 = draw_sequence(2600)
+df_after_burn_in = df_v2[600:].reset_index()
 
+df_thinned = df_after_burn_in[df_after_burn_in['index'] % 20 == 0].drop(['index'], axis=1)
+marginal_R_T_burned_and_thinned = df_thinned['rain'].value_counts(normalize=True)['T']
 
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 8.\n')
+    outfile.write('P(R=T|S=T,W=T)_burned_and_thinned = {:.2f}\n'.format(marginal_R_T_burned_and_thinned))
+    outfile.write('We will wait for next task in order to compare this with  "P(R=T|S=T,W=T)_sample100". \n\n')
 
+"""#######################################################################################
+9. Compute probability analitically
+#######################################################################################"""
+nominator_components = model.probability([
+    ['T', 'T', 'T', 'T'],
+    ['T', 'F', 'T', 'T']
+])
 
+denominator_components = model.probability([
+    ['T', 'T', 'T', 'T'],
+    ['T', 'F', 'T', 'T'],
+    ['F', 'T', 'T', 'T'],
+    ['F', 'F', 'T', 'T']
+])
+marginal_R_T_true = sum(nominator_components) / sum(denominator_components)
 
-
-
-""" testowe prawdopodobienstwa"""
-# def get_prob(drawing_var_name, second_var_state):
-#     return {
-#         'rain': {
-#             'T': 0.2,
-#             'F': 0.3
-#         },
-#         'cloudy': {
-#             'T': 0.4,
-#             'F': 0.5
-#         }
-#     }[drawing_var_name][second_var_state]
-""" sprawdzenie, czy procenty sa takie, jak ustalone wczesniej (dopiero przy 1000 jest ok)"""
-# df[df['cloudy'] == 'T']['rain'].value_counts(normalize=True)['T']
-# df[df['cloudy'] == 'F']['rain'].value_counts(normalize=True)['T']
-# df[df['rain'] == 'T']['cloudy'].value_counts(normalize=True)['T']
-# df[df['rain'] == 'F']['cloudy'].value_counts(normalize=True)['T']
-"""rzeczywiscie pytaja o kowariancje"""
-# test_df = pd.DataFrame.from_dict({
-#     'x1': [64.0, 66.0, 68.0, 69.0, 73.0],
-#     'x2': [580.0, 570.0, 590.0, 660.0, 600.0],
-#     'x3': [29.0, 33.0, 37.0, 46.0, 55.0]
-# })
-#
-# sum_ = np.zeros((3, 3))
-# for idx in range(5):
-#     x_dash = test_df.mean().values
-#     x_i = test_df.iloc[[idx]].values
-#     sum_ += (x_i - x_dash).T @ (x_i - x_dash)
-#
-# print(sum_ / 4)
-# print(test_df.cov())
+with open(text_output_path, 'a') as outfile:
+    outfile.write('Task 9.\n')
+    outfile.write('P(R=T|S=T,W=T)_true = {:.2f}\n'.format(marginal_R_T_true))
+    outfile.write('As a summary we can compare probabilities from 2 estimation methods:\n')
+    outfile.write('P(R=T|S=T,W=T)_sample100 = {:.2f}\n'.format(marginal_R_T_sample100))
+    outfile.write('P(R=T|S=T,W=T)_burned_and_thinned = {:.2f}\n'.format(marginal_R_T_burned_and_thinned))
+    outfile.write('As a conclusion I can say, that adding burning-in and thinning gave us better estimate.\n')
 
 
