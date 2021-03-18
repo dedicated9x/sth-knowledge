62299fefda65447e66c96247794f8edb62ccf049
Brysch Pawel <pawel.brysch@gmail.com>
2021-03-18 10:42:22 +0100

    Removed ffmped dir.

--- mini_projects/claus/lib/paths_registry.py
+++ mini_projects/claus/lib/paths_registry.py
@@ -3,5 +3,7 @@ import os
 
 class PathsRegistry:
     klaus = pl.Path(os.environ["CLAUS"])
-    ffmpeg = pl.Path(__file__).parent.parent.joinpath("ffmpeg").joinpath("bin").joinpath("ffmpeg.exe")
+    # ffmpeg = pl.Path(__file__).parent.parent.joinpath("ffmpeg").joinpath("bin").joinpath("ffmpeg.exe")
+    ffmpeg = pl.Path(rf"C:\Users\devoted\Documents\bin\ffmpeg\bin\ffmpeg.exe")
     temp = pl.Path(__file__).parent.parent.joinpath('temp')
+
