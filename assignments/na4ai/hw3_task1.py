f23a993d2cfab410c2a9c903eccb3e656d187b12
Brysch Pawel <pawel.brysch@gmail.com>
2021-04-06 17:52:42 +0200

    hw3_task2done

--- assignments/na4ai/hw3_task1.py
+++ assignments/na4ai/hw3_task1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pathlib as pl
 import matplotlib.pyplot as plt
-import functools
 
 
 def polynomial_value(beta, x: float):
@@ -12,74 +11,70 @@ def polynomial_value(beta, x: float):
 # calculate_polynomial_value(np.array([4., 5., 6.])[np.newaxis, :], 2.) # 38 = 4*2^0 + 5*2^1 + 6*2^2
 
 
-def calculate_polynomial_regression_gradient(xs, ys, params_vec):
-    N = xs.shape[0]
-    p = params_vec.shape[1] - 1
+class PolynomialRegressionLossFunction:
+    def __init__(self, xs, ys):
+        self._xs = xs
+        self._ys = ys
 
-    ys_pred = polynomial_value(params_vec, xs)
-    partial_gradients = []
-    for j in np.arange(p + 1):
-        # a = (-2 / N)
-        # b = (xs ** j).T
-        # c = (ys - ys_pred)
-        # d = b @ c
-        # dMSE_dMj = a * d
-        dMSE_dMj = (-2 / N) * (xs ** j).T @ (ys - ys_pred)
-        partial_gradients.append(dMSE_dMj[0][0])
+    def calculate_gradient(self, beta):
+        N = xs.shape[0]
+        p = beta.shape[1] - 1
 
-    gradient = np.array(partial_gradients).reshape(1, p + 1)
-    return gradient
+        ys_pred = polynomial_value(beta, xs)
+        partial_gradients = []
+        for j in np.arange(p + 1):
+            dMSE_dMj = (-2 / N) * (self._xs ** j).T @ (self._ys - ys_pred)
+            partial_gradients.append(dMSE_dMj[0][0])
+
+        gradient = np.array(partial_gradients).reshape(1, p + 1)
+        return gradient
 
 
-def minimize_gradient_descent(gradient_func, params_vec_0, alpha):
-    params_vec = params_vec_0
-    log = []
-    for i in range(20000000):
-        gradient = gradient_func(params_vec)
-        params_vec = params_vec - alpha * gradient
-        gradient_norm = np.linalg.norm(gradient)
-        log.append(gradient_norm)
-        if gradient_norm < 10e-5:
+def gradient_descent(beta0, gradient_func, alpha, fitness_func=None, fitness_limit=None, break_limit=int(2e7)):
+    beta = beta0
+    fitness_log = []
+    has_converged = False
+    for i in range(break_limit):
+        beta_grad = gradient_func(beta)
+        beta = beta - alpha * beta_grad
+        fitness = fitness_func(beta, beta_grad)
+        fitness_log.append(fitness)
+        if fitness < fitness_limit:
+            has_converged = True
             break
-    return params_vec, log
+    return beta, has_converged, fitness_log
 
 
 def polynomial_regression_gradient_descent(gradient_func, p, alpha):
-    params_vec_0 = np.random.randn(1, p + 1)
-    params_vec, _ = minimize_gradient_descent(gradient_func, params_vec_0, alpha)
-    return params_vec
+    beta0 = np.random.randn(1, p + 1)
+    fitness_func = lambda beta, beta_grad: np.linalg.norm(beta_grad)
+    beta, _, _ = gradient_descent(beta0, gradient_func, alpha, fitness_func, fitness_limit=1e-5)
+    return beta
 
 
 
+if __name__ == "__main__":
+    df = np.load(pl.Path(rf"C:\Users\devoted\Downloads\assignment22.npy"))
+    xs = df[:, 0][:, np.newaxis]
+    ys = df[:, 1][:, np.newaxis]
+    gradient_func = PolynomialRegressionLossFunction(xs, ys).calculate_gradient
 
 
-df = np.load(pl.Path(rf"C:\Users\devoted\Downloads\assignment22.npy"))
-xs = df[:, 0][:, np.newaxis]
-ys = df[:, 1][:, np.newaxis]
-gradient_func = functools.partial(calculate_polynomial_regression_gradient, xs, ys)
-
-"""MAIN"""
-# P = 5
-# params_vec_03 = np.random.randn(1, P + 1)
-# params_vec_3, log_3 = minimize_gradient_descent(gradient_func, params_vec_03, alpha=0.000001)
+    vecs = [
+        polynomial_regression_gradient_descent(gradient_func, 1, 0.001),
+        polynomial_regression_gradient_descent(gradient_func, 2, 0.001),
+        # polynomial_regression_gradient_descent(gradient_func, 3, 0.0001),
+        # polynomial_regression_gradient_descent(gradient_func, 5, 0.000001)
+    ]
 
 
+    def plot_results(vecs_, xs_, ys_):
+        x_grid = np.linspace(xs_.min(), xs_.max(), 500)[:, np.newaxis]
+        fig, ax = plt.subplots()
+        for params_vec in vecs_:
+            ys_pred = polynomial_value(params_vec, x_grid)
+            ax.scatter(x_grid, ys_pred, marker='.', linewidths=1)
+        ax.scatter(xs_, ys_)
 
-"""test"""
-def plot_results(vecs_, xs_, ys_):
-    x_grid = np.linspace(xs_.min(), xs_.max(), 500)[:, np.newaxis]
-    fig, ax = plt.subplots()
-    for params_vec in vecs_:
-        ys_pred = polynomial_value(params_vec, x_grid)
-        ax.scatter(x_grid, ys_pred, marker='.', linewidths=1)
-    ax.scatter(xs_, ys_)
+    plot_results(vecs, xs, ys)
 
-vecs = [
-    polynomial_regression_gradient_descent(gradient_func, 1, 0.001),
-    polynomial_regression_gradient_descent(gradient_func, 2, 0.001),
-    # polynomial_regression_gradient_descent(gradient_func, 3, 0.0001),
-    # polynomial_regression_gradient_descent(gradient_func, 5, 0.000001)
-]
-plot_results(vecs, xs, ys)
-
-# TODO sprawdzenie tej funkcji, czy to na pewno macierz 50x50
--- assignments/na4ai/hw3_task2.py
+++ assignments/na4ai/hw3_task2.py
@@ -1,3 +1,5 @@
+from assignments.na4ai.hw3_task1 import gradient_descent
+import matplotlib.pyplot as plt
 
 import numpy as np
 #function for generating random positive definite matrix
@@ -8,3 +10,16 @@ def get_randQ( n):
 
 DIM = 2500
 Q = get_randQ(DIM)
+
+
+beta, has_converged, fitness_log = gradient_descent(
+    beta0=np.random.randn(DIM)[:, np.newaxis],
+    gradient_func=lambda beta: (Q + Q.T) @ beta,
+    alpha=0.0001,
+    fitness_func=lambda beta, beta_grad: (beta.T @ Q @ beta)[0][0],
+    fitness_limit=50,
+    break_limit=int(2e3)
+)
+
+print(fitness_log[-1])
+plt.scatter(range(len(fitness_log)), fitness_log)
