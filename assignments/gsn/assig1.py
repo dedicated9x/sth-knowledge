da9975f3f31c64f800d2d3d371791fecf4a3127d
Brysch Pawel <pawel.brysch@gmail.com>
2021-04-22 10:50:21 +0200

    Before net refactor

--- assignments/gsn/assig1.py
+++ assignments/gsn/assig1.py
@@ -6,67 +6,37 @@ from torch.nn.parameter import Parameter
 from torch.nn import init
 import torchvision
 import torchvision.transforms as transforms
+import sys
 
-''''
-Tasks:
-1. Check that the given implementation reaches 95% test accuracy for
-   architecture input-64-64-10 in a few thousand batches.
 
-2. Improve initialization and check that the network learns much faster
-   and reaches over 97% test accuracy.
-
-3. Check, that with proper initialization we can train architecture
-   input-64-64-64-64-64-10, while with bad initialization it does
-   not even get off the ground.
-
-4. Add dropout implemented in pytorch
-
-5. Check that with 10 hidden layers (64 units each) even with proper
-    initialization the network has a hard time to start learning.
-
-6. Implement batch normalization (use train mode also for testing
-       - it should perform well enough):
-    * compute batch mean and variance
-    * add new variables beta and gamma
-    * check that the networks learns much faster for 5 layers
-    * check that the network learns even for 10 hidden layers.
-
-Bonus task:
-
-Design and implement in pytorch (by using pytorch functions)
-   a simple convnet and achieve 99% test accuracy.
-
-Note:
-This is an exemplary exercise. MNIST dataset is very simple and we are using
-it here to get resuts quickly.
-To get more meaningful experience with training convnets use the CIFAR dataset.
-'''
-
-"""to po prostu inicjalizuje wagi z rokładu normalnego (jakiegoś truncated)"""
 def truncated_normal_(tensor, mean=0, std=1):
     size = tensor.shape
+
+    # TODO
     tmp = tensor.new_empty(size + (4,)).normal_()
+    # tmp = tensor.new_empty(size + (4,)).normal_(generator=torch.Generator().manual_seed(1234567890))
+
+    # print(tmp)
+    # sys.exit()
+    # torch.Generator().manual_seed(2147483647)
+    # torch.Generator().manual_seed(1234567890)
+
+
     valid = (tmp < 2) & (tmp > -2)
     ind = valid.max(-1, keepdim=True)[1]
     tensor.data.copy_(tmp.gather(-1, ind).squeeze(-1))
     tensor.data.mul_(std).add_(mean)
 
-"""'Linear', bo przecież, jak dane przechodzą przez sieć, to jest to zwykłe przekształcenie liniowe"""
 class Linear(torch.nn.Module):
     """in_features, out_features -> <liczba> neuronów przed, <liczba> neuronów po tej warstwie."""
     def __init__(self, in_features, out_features):
         super(Linear, self).__init__()
-        # self.in_features = in_features
-        # self.out_features = out_features
-        """'Parameter' to 'Tensor', który zauważa, że jest atrybutem dla 'Module' i wpisuje się mu w odpowiednie miejsca przy inicjalizacji."""
         self.weight = Parameter(torch.Tensor(out_features, in_features))
         self.bias = Parameter(torch.Tensor(out_features))
         self.reset_parameters()
 
-    """User-defined funckją używana <tylko> przy inicjalizacji warstwy (do zainicjalizowania wartości wag)."""
+
     def reset_parameters(self):
-        # init.kaiming_normal_(self.weight, mode='fan_in') # dobra inicjalizacja dla Relu (95->97) # ma znaczene przy wielu warstawch
-        # init.xavier_normal_(self.w) -> druga opcja
         truncated_normal_(self.weight, std=0.5)
         init.zeros_(self.bias)
 
@@ -81,26 +51,17 @@ class Linear(torch.nn.Module):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        """
-        Linear to sieć/warstwa zaimplementowa powyżej.
-        Ad. 'sieć/warstwa' w ogólności sieci i warstwy mają taki sam interfejs (input + output). Stąd zapewne implementacja, w której dziedziczymy z tej samej klasy.
-        """
         self.fc1 = Linear(784, 64)
         self.fc2 = Linear(64, 64)
         self.fc3 = Linear(64, 10)
-
-        # self.dropout = nn.Dropout(0.25)
 
     """x -> inputy (wchodzą w petli SGD)"""
     def forward(self, x):
         """.view() to .reshape() dla tensorów"""
         x = x.view(-1, 28 * 28)
-        # x = self.dropout(x)
         """nn.Model.__call__() odpala .forward()"""
         x = F.relu(self.fc1(x))
-        # x = self.dropout(x)
         x = F.relu(self.fc2(x))
-        # x = self.dropout(x)
         x = self.fc3(x)
         return x
 
@@ -109,7 +70,9 @@ MB_SIZE = 128
 
 
 class MnistTrainer(object):
-    def __init__(self):
+    def __init__(self, net, no_epoch=20):
+        self.net = net
+        self.no_epoch = no_epoch
         transform = transforms.Compose(
                 [transforms.ToTensor()])
         self.trainset = torchvision.datasets.MNIST(
@@ -129,13 +92,14 @@ class MnistTrainer(object):
 
     def train(self):
         """net -> to prostu nasza sieć (nn.Model)"""
-        net = Net()
+        # net = Net()
+        net = self.net
         """criterion -> zwykła funkcja, której użyjemy później"""
         criterion = nn.CrossEntropyLoss()
         """FOCUS: sgd dostaje info o sieci, jaką będzie trenował"""
         optimizer = optim.SGD(net.parameters(), lr=0.05, momentum=0.9)
 
-        for epoch in range(20):
+        for epoch in range(self.no_epoch):
             running_loss = 0.0
             for i, data in enumerate(self.trainloader, 0):
                 inputs, labels = data
@@ -153,7 +117,6 @@ class MnistTrainer(object):
                 """loss w tym momencie to Tensor(1,1,1). Tensory tej kategorii mają metodę .item(), która zwraca ich wartość."""
                 """+= -> bo robimy <stochastic> GD (średnie, sumy, itd.)"""
                 running_loss += loss.item()
-                """w innych interwałach pokazujemy <loss>, a w innych <accuracy> (te drugie jest dalej) """
                 if i % 100 == 99:
                     print('[%d, %5d] loss: %.3f' %
                           (epoch + 1, i + 1, running_loss / 100))
@@ -162,12 +125,6 @@ class MnistTrainer(object):
             total = 0
             """torch.no_grad() -> cntxmngr, który zapewnia, że nic nie odpali .backward() (takie zabezpieczenie)"""
             with torch.no_grad():
-                #WPISAC GDZIE INDZIEJ W KODZIE: torch.is_grad_enabled() # sprawdza, czy jestesmy w torch.no_grad
-
-
-                # net.eval() <- np. dla batch normalizacji
-                # Ta funkcja sprawia, że cała sieć przechodzi w tryb ewaluacji (zamist teeningowe)
-                # np. wyłączy dropouta.
                 for data in self.testloader:
                     images, labels = data
                     outputs = net(images)
@@ -175,16 +132,36 @@ class MnistTrainer(object):
                     total += labels.size(0)
                     """do 'correct' dodajemy 0 lub 1, jednak jest to zapisany w nieintuicyjny sposób [sum() == WTF]"""
                     correct += (predicted == labels).sum().item()
-                # net.train() -> wracamy do trybu treningowego
 
             print('Accuracy of the network on the {} test images: {} %'.format(
                 total, 100 * correct / total))
 
 
 def main():
-    trainer = MnistTrainer()
+    net_base = Net()
+    trainer = MnistTrainer(net=net_base, no_epoch=4)
     trainer.train()
 
 
+"""
+[1,   100] loss: 2.583
+[1,   200] loss: 0.628
+[1,   300] loss: 0.504
+[1,   400] loss: 0.433
+Accuracy of the network on the 10000 test images: 88.87 %
+"""
+
+# TODO wrzucamy to jako argument na starcie (moze wywalimy 'neta' od razu, by nie bylo przekazywania po 10 razy)
+# TODO sprawdzamy, czy w long-runie jest 95% (amy nie miec "pechowych wag"
+    # TODO weights - zacznijmy od tego
+
+# TODO teraz pewnie szuffle
+
+# TODO ustawic seeda i zrobic zapis
+
+# TODO wczytaj, jak dataset, a nie jak mnista
+
 if __name__ == '__main__':
     main()
+
+
