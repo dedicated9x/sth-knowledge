297aa5a3b2c97bc7aae0d2dd279ed7bcba6e3a82
Brysch Pawel <pawel.brysch@gmail.com>
2021-04-22 15:35:15 +0200

    W trakcie tworzenia datasetu.

--- assignments/gsn/assig1.py
+++ assignments/gsn/assig1.py
@@ -98,6 +98,7 @@ class MnistTrainer(object):
 
                 """nn.Model.__call__() odpala .forward()"""
                 outputs = net(inputs)
+                """label(1), output(10)"""
                 loss = criterion(outputs, labels)
                 """loss to torch.Tenser. Stąd (kozacka) metoda .backward()"""
                 loss.backward()
@@ -148,9 +149,86 @@ Accuracy of the network on the 10000 test images: 91.72 %
 """
 
 
-# TODO wczytaj, jak dataset, a nie jak mnista
 
 if __name__ == '__main__':
     main()
 
+transform = transforms.Compose([transforms.ToTensor()])
+ts0 = torchvision.datasets.MNIST(root=rf"C:\Datasets", download=True, train=True, transform=transform)
+
+import pandas as pd
+import pathlib as pl
+from torch.utils.data import Dataset
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, root, transform=None, target_transform=None):
+        self.img_dir = pl.Path(root).joinpath('data')
+        self.img_labels = pd.read_csv(self.img_dir.joinpath('labels.csv'))
+        self.transform = transforms.Compose([transforms.ToTensor()])
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        # TODO
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        sample = {"image": image, "label": label}
+        return sample
+
+self = CustomImageDataset(rf"C:\Datasets\mnist_train")
+
+idx = 2
+
+# TODO stworzyc taki dataset i porownac
+# TODO solucja -> https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
+
+# TODO poczytac doksy od datasetu i loadera
+# TODO zerknac na slacka
+# TODO zobaczyc, jakie metody datasetu sa wolane ( w kodzie, ew. dodac dekorator)
+
+
+"""csv train"""
+# trainer = MnistTrainer(net=Net(), no_epoch=1)
+# ser_names = [e.name for e in pl.Path(rf"C:\Datasets\mnist_bad").glob('**/*')]
+# ser_labels = trainer.trainset.test_labels.numpy()
+# df = pd.DataFrame().assign(name=ser_names, label=ser_labels)
+# df.to_csv(rf"C:\temp\gsn_assig_1_output\label.csv", index=False)
+
+"""60000 zapis"""
+# for i in range(60000):
+#     arr = trainer.trainset.train_data[i, :, :].numpy()
+#     # arr = simpler(arr)
+#     filename = f'img_{str(i).zfill(5)}.png'
+#     cv2.imwrite(path_root.joinpath(filename).__str__(), arr)
+#
+#     print(i)
+
+
+"""MNIST vs GSN - testy"""
+# mnist_out = trainer.trainset.train_data[0, :, :].numpy()
+# mnist_out = np.vectorize(simpler)(mnist_out)
+# cv2.imwrite(path_root.joinpath('mnist.png').__str__(), mnist_out)
+#
+# ex_gsn = cv2.imread(path_root.joinpath('gsn.png').__str__()) # bit depth = 8
+# ex_mnist = cv2.imread(path_root.joinpath('mnist.png').__str__()) # bit depth = 32
+#
+# gsn0 = ex_gsn[:, :, 0]
+# gsn1 = ex_gsn[:, :, 1]
+# gsn2 = ex_gsn[:, :, 2]
+#
+# mnist0 = ex_mnist[:, :, 0]
+# mnist1 = ex_mnist[:, :, 1]
+# mnist2 = ex_mnist[:, :, 2]
+#
+# summary(gsn0)
+# summary(mnist0)
+# summary(mnist_out)
 
